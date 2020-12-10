285f0bd45b83f4b0a05ec9fa3266b67ca28369da
rezita11 <68811466+rezita11@users.noreply.github.com>
2020-12-10 16:36:04 +0700

    Add files via upload

---
+++ Tipedata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipedata;
+
+/**
+ *
+ * @author asus
+ */
+public class Tipedata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama, alamat;
+        Double latitude, longitude;
+        nama = "Rezita Cahya Adhina";
+        alamat = "Kauman, Sreangat, Blitar";
+        latitude = 112.0764929;
+        longitude = -8.0787287;
+        
+        System.out.println("nama :" +nama);
+        System.out.println("alamat :" +alamat);
+        System.out.println("latitude :" +latitude);
+        System.out.println("longitude :" +longitude);
+        
+      
+    }
+    
+}
